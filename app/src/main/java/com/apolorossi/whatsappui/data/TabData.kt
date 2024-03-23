18badc9f05e69628cbd0efe4b95cb095688bc81a
Apolo  Rossi <apolorossi@Apolos-MBP.local>
2024-03-23 15:53:32 -0300

    feat: Created TabComponent.

---
+++ app/src/main/java/com/apolorossi/whatsappui/data/TabData.kt
@@ -0,0 +1,19 @@
+package com.apolorossi.whatsappui.data
+
+data class TabData(
+    val title: String,
+    val unreadCount: Int?
+)
+
+
+val tabs = listOf(
+    TabData(Tabs.CHATS.value, 5),
+    TabData(Tabs.STATUS.value, null),
+    TabData(Tabs.CALLS.value, 4)
+)
+
+enum class Tabs (val value:String) {
+    CHATS("Chats"),
+    STATUS("Status"),
+    CALLS("Calls")
+}
--- app/src/main/java/com/apolorossi/whatsappui/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/apolorossi/whatsappui/ui/screens/HomeScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.apolorossi.whatsappui.ui.components.AppBarComponent
+import com.apolorossi.whatsappui.ui.components.TabsComponent
 
 @Composable
 fun HomeScreen() {
     Column {
         AppBarComponent()
+        TabsComponent()
     }
 }
 
